7f480c3bfb1af43a81169be11071e3a2b91956c6
Nathan Seixeiro <108758693+nathanSeixeiro@users.noreply.github.com>
2023-09-11 17:11:46 +0000

    e

---
+++ task.ts
@@ -0,0 +1,46 @@
+export class Task {
+    private readonly _userId!: string | number 
+    private nameTask: string
+    private descriptionTask: string
+    private dateTask: Date
+    private completedTask: boolean = false
+
+    
+    constructor(nameTask: string, descriptionTask: string, dateTask: Date) { 
+        this.nameTask = nameTask
+        this.descriptionTask = descriptionTask
+        this.dateTask = dateTask
+    }
+
+    get name(): string {
+        return this.nameTask
+    }
+
+    get description(): string {
+        return this.descriptionTask
+    }
+
+    get date(): Date {
+        return this.dateTask
+    }
+
+    get completed(): Boolean{
+        return this.completed
+    }
+
+    set name(name: string){
+        this.nameTask = name
+    }
+    set description(desc: string){
+        this.descriptionTask = desc
+    }
+
+    set date(date: Date){
+        this.dateTask = date
+    }
+
+    set completed(status: boolean){
+        this.completed = status
+    }
+
+}
