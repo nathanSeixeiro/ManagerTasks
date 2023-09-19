3121575ef54fb5d5eac054ae82ba5baac1d5238a
Nathan Seixeiro <108758693+nathanSeixeiro@users.noreply.github.com>
2023-09-19 16:45:55 +0000

    refactor: change folder name

--- __test__/use-cases/create-user.test.ts
+++
@@ -1,8 +0,0 @@
-import { Task } from "@/domain/entities";
-
-describe('create usecase', () => {
-    const makeSut = () => {
-        const sut = new Task('test','test instance', new Date() )
-        return sut
-    }
-})
